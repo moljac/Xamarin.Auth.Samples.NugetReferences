f987a79f310afc4219f94911c3a2b9da548b33c2
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2017-05-24 12:33:40 +0200

    CustomTabs Customization API tests

--- Traditional.Standard/Providers/Xamarin.Auth.Sample.XamarinAndroid/MainActivity.cs
+++ Traditional.Standard/Providers/Xamarin.Auth.Sample.XamarinAndroid/MainActivity.cs
@@ -54,6 +54,17 @@ namespace Xamarin.Auth.Sample.XamarinAndroid
             // [Chrome] Custom Tabs WarmUp and prefetch
             custom_tab_activity_helper = new global::Android.Support.CustomTabs.Chromium.SharedUtilities.CustomTabActivityHelper();
 
+            global::Xamarin.Auth.CustomTabsConfiguration.ActionLabel = null;
+            global::Xamarin.Auth.CustomTabsConfiguration.MenuItemTitle = null;
+            global::Xamarin.Auth.CustomTabsConfiguration.AreAnimationsUsed = true;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsShowTitleUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsUrlBarHidingUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsCloseButtonIconUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsActionButtonUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsActionBarToolbarIconUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsDefaultShareMenuItemUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.ToolbarColor = global::Android.Graphics.Color.Orange;
+            
             return;
         }
 
