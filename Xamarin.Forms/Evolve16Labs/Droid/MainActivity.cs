1ba971ce782f904d2c847a1846690e722c933d81
Miljenko Cvjetko moljac moki mel <mcvjetko@holisticware.net>
2017-05-24 12:32:22 +0200

    CustomTabs customizations

--- Xamarin.Forms/Evolve16Labs/Droid/MainActivity.cs
+++ Xamarin.Forms/Evolve16Labs/Droid/MainActivity.cs
@@ -21,6 +21,14 @@ namespace ComicBook
 
             global::Xamarin.Auth.CustomTabsConfiguration.ActionLabel = null;
             global::Xamarin.Auth.CustomTabsConfiguration.MenuItemTitle = null;
+            global::Xamarin.Auth.CustomTabsConfiguration.AreAnimationsUsed = true;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsShowTitleUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsUrlBarHidingUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsCloseButtonIconUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsActionButtonUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsActionBarToolbarIconUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.IsDefaultShareMenuItemUsed = false;
+            global::Xamarin.Auth.CustomTabsConfiguration.ToolbarColor = global::Android.Graphics.Color.Orange;
 
             LoadApplication(new App());
         }
